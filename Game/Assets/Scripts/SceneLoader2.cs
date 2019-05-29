6b39ebb451cb056fe978269d35b8ea9829d7d497
WoolfyDuh <14461196+WoolfyDuh@users.noreply.github.com>
2019-05-29 14:40:55 +0200

    added button
    added start button

---
+++ Game/Assets/Scripts/SceneLoader2.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class SceneLoader2 : MonoBehaviour
+{
+
+	public void LoadGame()
+	{
+		SceneManager.LoadScene("Game");
+		Debug.Log("PineApples");
+	}
+}
+
