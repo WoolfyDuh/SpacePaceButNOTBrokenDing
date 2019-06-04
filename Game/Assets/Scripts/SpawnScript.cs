7f8234ac8054fda11c4426383a6bd2023a45298e
Jorikos <26204@ma-web.nl>
2019-06-04 14:05:46 +0200

    debug logs weggehaald

--- Game/Assets/Scripts/SpawnScript.cs
+++ Game/Assets/Scripts/SpawnScript.cs
@@ -8,31 +8,32 @@ public class SpawnScript : MonoBehaviour
     [SerializeField] Transform rechterborder; // Transforms voor alle grenzen
     [SerializeField] Transform linkerborder;
     [SerializeField] Transform bovenborder;
-    [SerializeField] Transform onderborder; 
+    [SerializeField] Transform onderborder;
+    public GameObject charger;
+    public GameObject shooter;
     // Start is called before the first frame update
     void Start()
     {
-        
     }
-
     // Update is called once per frame
     void Update()
     {
-        
+        Wait();
     }
 
     IEnumerator Wait() // functie om te wachten
     {
-
+        SpawnCharger();
         yield return new WaitForSecondsRealtime(30);
-        
+        SpawnShooter();
     }
 
     void SpawnCharger()
     {
+        Instantiate(charger, transform.position, Quaternion.identity);
     }
     void SpawnShooter()
     {
-
+        Instantiate(shooter, transform.position, Quaternion.identity);
     }
 }
