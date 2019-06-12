d1bb8843f64125e1e37568a14079a9188e88f322
WoolfyDuh <14461196+WoolfyDuh@users.noreply.github.com>
2019-06-12 16:08:47 +0200

    added looping map
    looper dooper poopers

---
+++ Game/Assets/Scripts/LeftBorderScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LeftBorderScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+	private void OnTriggerEnter2D(Collider2D other)
+	{
+
+		if (other.CompareTag("Player"))
+		{
+			float yPos = other.gameObject.transform.position.y;
+			other.transform.position = new Vector3(290, yPos, 0);
+			Debug.Log("krijg de tering jamiro");
+		}
+	}
+}
---
+++ Game/Assets/Scripts/RightBorderScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RightBorderScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+	private void OnTriggerEnter2D(Collider2D other)
+	{
+		
+	if (other.CompareTag("Player"))
+		{ float yPos = other.gameObject.transform.position.y;
+			other.transform.position = new Vector3(-290,yPos,0);
+			Debug.Log("krijg de tering jamiro");
+		}	
+	}
+}
+
