d89c9e3f0994d843e6b61baa4dab76b8cce5c943
Jorikos <26204@ma-web.nl>
2019-06-12 09:28:16 +0200

    Asteroid prefabs

--- Game/Assets/Scripts/AsteroidScript.cs
+++ Game/Assets/Scripts/AsteroidScript.cs
@@ -10,8 +10,9 @@ public class AsteroidScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-
-
+        astroids[0] = Resources.Load<GameObject>("Prefabs/AsteroidPiece1");
+        astroids[1] = Resources.Load<GameObject>("Prefabs/AsteroidPiece2");
+        astroids[2] = Resources.Load<GameObject>("Prefabs/AsteroidPiece3");
     }
 
     // Update is called once per frame
@@ -23,14 +24,14 @@ public class AsteroidScript : MonoBehaviour
     {
         for (int i = 0; i < 2; i++)
         {
-            Vector2 tempVec = (Vector2)transform.position + Vector2.right * 5 * i; // temporary vector = vector 2, positie van de asteroid plus vector 2 right keer 5 keer i
-            Instantiate(astroids[i], tempVec, Quaternion.identity);
+            Vector2 tempVec = (Vector2)transform.position + Vector2.right * i; // temporary vector = vector 2, positie van de asteroid plus vector 2 right keer 5 keer i
+            Instantiate(astroids[Random.Range(0,2)], tempVec, Quaternion.identity);
             Destroy(gameObject);
         }
         for (int i = 0; i < 2; i++)
         {
-            Vector2 tempVec = (Vector2)transform.position + Vector2.down * 5 + Vector2.right * 5 * i; // temporary vector = vector 2, positie van de asteroid plus vector 2 right keer 5 keer i
-            Instantiate(astroids[i], tempVec, Quaternion.identity);
+            Vector2 tempVec = (Vector2)transform.position + Vector2.down + Vector2.right * 5 * i; // temporary vector = vector 2, positie van de asteroid plus vector 2 right keer 5 keer i
+            Instantiate(astroids[Random.Range(0,2)], tempVec, Quaternion.identity);
             Destroy(gameObject);
         }
     }
